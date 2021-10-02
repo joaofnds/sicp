5598845f3cbe61196e4feda27973b6949b241d5c
João Fernandes <joaofnds@joaofnds.com>
2021-10-02 13:05:12 -0300

    fix last-pair

--- 2.2.1-representing-sequences.scm
+++ 2.2.1-representing-sequences.scm
@@ -39,10 +39,10 @@
 
 (define (last-pair items)
   (if (null? (cdr items))
-      (car items)
+      items
       (last-pair (cdr items))))
 
-(last-pair (list 23 72 149 34)) ;; => 34
+(last-pair (list 23 72 149 34)) ;; => (34)
 
 
 (define (reverse items)
